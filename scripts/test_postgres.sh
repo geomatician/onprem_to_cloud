38b50c7dcdf372fd0b20fc9b8f66b0b222654627
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2026-05-11 16:57:36 -0500

    update

--- scripts/test_postgres.sh
+++ scripts/test_postgres.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-psql -h host.docker.internal -U postgres -p 5432 -d pagila -c "SELECT COUNT(*) FROM customer;"
+psql -h host.docker.internal -U dms_user -p 5432 -d pagila -c "SELECT COUNT(*) FROM customer;"
