0f102e51aa7467c046e76b5a9d7ec108b53aacd2
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2026-05-11 19:42:03 -0500

    update

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,11 +1,11 @@
-output "dms_role_arn" {
-  value = aws_iam_role.dms_role.arn
+output "dms_s3_role_arn" {
+  value = aws_iam_role.dms_s3_role.arn
 }
 
 output "redshift_role_arn" {
   value = aws_iam_role.redshift_role.arn
 }
 
-output "dms_s3_role_arn" {
-  value = aws_iam_role.dms_s3_role.arn
+output "dms_vpc_role_arn" {
+  value = aws_iam_role.dms_vpc_role.arn
 }
