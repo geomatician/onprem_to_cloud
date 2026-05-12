daeb5c359d609b518551a95424cb7a5476d81599
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2026-05-11 19:52:16 -0500

    update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,16 +8,18 @@ resource "aws_vpc" "this" {
   }
 }
 
-resource "aws_subnet" "public" {
-  count = 2
-
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+}
 
-  tags = {
-    Name = "public-${count.index}-${var.environment}"
-  }
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
