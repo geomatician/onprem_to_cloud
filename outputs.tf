3db73e6c465c19dc236ad5fd1bbe8c548d498f89
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2026-05-12 09:21:35 -0500

    update

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "redshift_endpoint" {
   value = module.redshift.endpoint
 }
 
+output "redshift_role_arn" {
+  value = module.iam.redshift_role_arn
+}
+
 output "bucket_name" {
   value = module.s3.bucket_name
 }
@@ -9,7 +13,3 @@ output "bucket_name" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-
-# output "dms_task_arn" {
-#   value = module.dms.task_arn
-# }
